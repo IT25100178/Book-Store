cabdef6afcbfd03e0c78a4ffe425df1f1ca6ce27
IT25101270 <it25101270@my.sliit.lk>
2026-04-17 19:19:22 +0530

    components

---
+++ src/components/checkout/PlaceOrderButton.jsx
@@ -0,0 +1,9 @@
+function PlaceOrderButton() {
+  return (
+    <div>
+      <h2>Place Your Order</h2>
+      <button>place my order</button>
+    </div>
+  );
+}
+export default PlaceOrderButton;
---
+++ src/components/checkout/TaxCalculation.jsx
@@ -0,0 +1,9 @@
+function TaxCalculation() {
+  return (
+    <div>
+      <h2>Tax Calculation</h2>
+      <button>Calculate Tax</button>
+    </div>
+  );
+}
+export default TaxCalculation;
