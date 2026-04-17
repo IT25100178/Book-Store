ab88e7c58f6c61d44928cc316a54e4fa8351afa0
IT25101270 <it25101270@my.sliit.lk>
2026-04-17 18:52:11 +0530

    Components

---
+++ src/components/checkout/PaymentMethodSelection.jsx
@@ -0,0 +1,10 @@
+function PaymentMethodSelection() {
+  return (
+    <div>
+      <h2>Payment Method Selection</h2>
+      <button>Cash on delivery</button>
+      <button>Pay online</button>
+    </div>
+  );
+}
+export default PaymentMethodSelection;
